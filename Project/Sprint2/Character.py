a8b3990fbf4531a55ae01e54dd6098aab34b3a30
g-vohra <garv.vohra21@gmail.com>
2024-04-30 21:31:34 +1000

    added Character class

---
+++ Project/Sprint2/Character.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class CharacterImage(Enum):
+    BAT = 'Assets/BabyBat.png'
+    BABY_DRAGON = 'Assets/BabyDragon.png'
+    SPIDER = 'Assets/BabySpider.png'
+    SALAMANDER = 'Assets/Salamander.png'
+    PIRATE = 'Assets/Cave.png'
