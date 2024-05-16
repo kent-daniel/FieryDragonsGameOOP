a7346719ceee6c2c311914abcd684e45b4b80fbb
GuntajS <gsin0055@student.monash.edu>
2024-05-16 16:06:41 +1000

    adding documentation and assigning the value of the number of squares to player

--- Project/Sprint 3/Win.py
+++ Project/Sprint 3/Win.py
@@ -5,6 +5,13 @@ from Player import Player
 
 class Win:
     def __init__(self, player: Player, colour=GameStyles.COLOR_GRAY_700_T.value):
+        """
+
+        :param player:
+        :param colour:
+        This initialises the constants of the win notification such as a surface for the notification to show on,
+        and the text size,font and color
+        """
         self.font = pygame.font.Font(GameStyles.FONT_WIN.value, GameStyles.FONT_SIZE_WIN.value)
         self.screen_width, self.screen_height = \
             pygame.display.get_desktop_sizes()[0]
@@ -13,7 +20,6 @@ class Win:
             GameStyles.COLOR_ORANGE.value, GameStyles.COLOR_BROWN_LIGHT.value)
         self.textRect = self.text.get_rect()
         self.textRect.center = (self.screen_width / 2, self.screen_height / 2)
-        # self.textrect.center =
         self.win_notif_surface: pygame.Surface = pygame.Surface(
             (self.screen_width, self.screen_height), pygame.SRCALPHA)
         pygame.draw.rect(self.win_notif_surface, colour,
@@ -22,6 +28,14 @@ class Win:
         self.rect: pygame.Rect = self.win_notif_surface.get_rect()
 
     def render_win(self, destination_surface, location):
+        """
+
+        :param destination_surface:
+        :param location:
+        :return: The win notification on the GUI
+
+        renders the surface on to the main screen then the text is rendered onto the surface
+        """
         self.win_notif_surface.blit(self.text,
                                     self.textRect)
         destination_surface.blit(self.win_notif_surface, location)
