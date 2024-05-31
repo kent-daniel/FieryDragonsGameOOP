17e05cfec339c120a6558478f7cbc39bd149ed47
GuntajS <gsin0055@student.monash.edu>
2024-05-31 19:30:22 +1000

    Start Game UI class finished

--- Project/Sprint 3/StartGameUI.py
+++ Project/Sprint 3/StartGameUI.py
@@ -16,13 +16,14 @@ class StartGameUI(Drawable):
                                          button_width, button_height)
         self.previous_game_rect = pygame.Rect(width // 2 - button_width // 2, height // 2 + spacing // 2, button_width,
                                               button_height)
+        pygame.draw.rect(self.start_game_surface, surface_colour, self.start_game_surface.get_rect())
         pygame.draw.rect(self.start_game_surface, button_colour,
                          self.new_game_rect,
                          border_radius=GameStyles.BORDER_RADIUS_SMALL.value)
-        pygame.draw.rect(self.start_game_surface, button_colour,
-                         self.previous_game_rect,
+        pygame.draw.rect(self.start_game_surface, button_colour, self.previous_game_rect,
                          border_radius=GameStyles.BORDER_RADIUS_SMALL.value)
         self.rect: pygame.Rect = self.start_game_surface.get_rect()
+
         self.redraw_view()
 
     def draw(self, destination_surface: pygame.Surface, location: Tuple[int, int]) -> None:
@@ -34,5 +35,12 @@ class StartGameUI(Drawable):
         pass
 
     def redraw_view(self) -> None:
-        pass
+        font = pygame.font.SysFont(None, GameStyles.FONT_SIZE_LARGE.value)
+        new_game_text = font.render("New Game", True, GameStyles.COLOR_BLACK.value)
+        previous_game_text = font.render("Previous Game", True, GameStyles.COLOR_BLACK.value)
+        self.start_game_surface.blit(new_game_text, new_game_text.get_rect(center=self.new_game_rect.center))
+        self.start_game_surface.blit(previous_game_text,
+                                     previous_game_text.get_rect(center=self.previous_game_rect.center))
+
+
 
