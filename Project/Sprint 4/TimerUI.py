6638c0095d5c0fb7cef02166001bfa4a93cb92de
GuntajS <gsin0055@student.monash.edu>
2024-06-06 17:47:48 +1000

    Adding the timer method using a controller to send information to PlayerTurnController.py and TimerUI, tested and correctly working on my end

--- Project/Sprint 4/TimerUI.py
+++ Project/Sprint 4/TimerUI.py
@@ -4,6 +4,7 @@ import pygame
 from Drawable import Drawable
 from GameConstants import GameStyles
 
+
 class TimerUI(Drawable):
     def __init__(self, timer_controller: TimerController, width=150,
                  height=150, colour=GameStyles.COLOR_TRANSPARENT.value):
@@ -11,13 +12,14 @@ class TimerUI(Drawable):
         self.screen_width, self.screen_height = pygame.display.get_desktop_sizes()[0]
         self._timer_controller = timer_controller
         self.timer_surface = pygame.Surface((width, height), pygame.SRCALPHA)
-        self.location = pygame.Rect(self.screen_width-width, self.screen_height-height,width,height)
+        self.location = pygame.Rect(self.screen_width - width, self.screen_height - height, width, height)
         pygame.draw.rect(self.timer_surface, colour,
                          self.location,
                          border_radius=GameStyles.BORDER_RADIUS_SMALL.value)
         self.rect: pygame.Rect = self.timer_surface.get_rect()
         self.redraw_view()
         self.timer_emoji = " Timer: "
+
     def draw(self, destination_surface: pygame.Surface, location: Tuple[int, int]) -> None:
         self.text = self.timer_emoji + self._timer_controller.get_time()
         self.redraw_view()
@@ -25,8 +27,9 @@ class TimerUI(Drawable):
 
     def get_surface(self):
         pass
+
     def redraw_view(self) -> None:
         font = pygame.font.SysFont(None, GameStyles.FONT_SIZE_LARGE.value)
-        text_surface = font.render(self.text, True, GameStyles.COLOR_BROWN_DARK.value, GameStyles.COLOR_BROWN_LIGHT.value)
-        self.timer_surface.blit(text_surface,(0,0,100,100))
-
+        text_surface = font.render(self.text, True, GameStyles.COLOR_BROWN_DARK.value,
+                                   GameStyles.COLOR_BROWN_LIGHT.value)
+        self.timer_surface.blit(text_surface, (0, 0, 100, 100))
