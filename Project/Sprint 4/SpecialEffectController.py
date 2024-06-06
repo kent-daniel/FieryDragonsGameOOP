54a73a3ec85e80a76dc7eae0283882d55b73c144
VanshBatas <vbat0005@student.monash.edu>
2024-06-06 18:19:58 +1000

    Steps to win logic for special effect works successfully

--- Project/Sprint 4/SpecialEffectController.py
+++ Project/Sprint 4/SpecialEffectController.py
@@ -24,6 +24,35 @@ class SpecialEffectController:
             current_player_square = self.location_manager.get_player_location(current_player)
             closest_player_square = self.location_manager.get_player_location(closest_player)
 
+            distance_forward = len(self.location_manager.get_closest_forward(current_player_square, closest_player_square))
+            distance_backward = len(self.location_manager.get_closest_backward(current_player_square, closest_player_square))
+            if distance_backward < distance_forward:
+                # if current player stepstowin > 26 then - 26 and opposite for closest
+                current_player.steps_to_win += distance_backward
+                closest_player.steps_to_win -= distance_backward
+                if current_player.steps_to_win > 26:
+                    current_player.steps_to_win -= 26
+                if closest_player.steps_to_win < 0:
+                    closest_player.steps_to_win += 26
+            else:
+                # if current player stepstowin < 0 then + 26 and opposite for closest
+                current_player.steps_to_win -= distance_forward
+                closest_player.steps_to_win += distance_forward
+                if current_player.steps_to_win < 0:
+                    current_player.steps_to_win += 26
+                if closest_player.steps_to_win > 26:
+                    closest_player.steps_to_win -= 26
+            # update player data
+
+            self.player_data_controller.update_player(current_player)
+            self.player_data_controller.update_player(closest_player)
+
+            # Remove the current player's location from the location manager
+            # Remove the closest player's location from the location manager
+            # Set the current player's location to the closest player's location
+            # Set the closest player's location to the current player's location
+            # Add a notification to the notification manager saying that the players were swapped
+
             self.location_manager.remove_player_location(current_player_square)
             self.location_manager.remove_player_location(closest_player_square)
 
@@ -49,15 +78,4 @@ class SpecialEffectController:
                 if distance < min_distance:
                     min_distance = distance
                     closest_player = player
-
         return closest_player
-
-    # def calculate_distance(self, tile1: Tile, tile2: Tile) -> int:
-    #     # squares = self.location_data_controller.get_squares()
-    #     # index1 = squares.index(tile1)
-    #     # index2 = squares.index(tile2)
-    #     #
-    #     # distance = min(abs(index1 - index2), len(squares) - abs(index1 - index2))
-    #     # return distance
-    #     distance = len(self.location_manager.get_tiles_between(tile1, tile2))
-    #     return distance
